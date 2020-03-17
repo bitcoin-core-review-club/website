ad61075672e8e1b0a60e037bd18424b138a9cae8
Jon Atack <jon@atack.com>
2020-03-17 23:55:57 +0100

    Convert auto_logs_markup plugin to callable filter
    as per Dave Harding suggestion, implemented by John Newbery.

--- _plugins/auto_logs_markup.rb
+++ _plugins/auto_logs_markup.rb
@@ -2,60 +2,74 @@
 
 require 'uri'
 
-# URI schemes to accept for extraction.
-URI_SCHEMES = %w(http https).freeze
+module Jekyll
+  class IRCBlock < Liquid::Block
+    # Convert IRC meeting logs from plain text to HTML with functional links
+    # and log lines by enclosing them in {% irc %} and {% endirc %} tags.
 
-# Trailing chars (all punctuation except "/") to remove from URIs.
-TRAILING = /[^\/[:^punct:]]+$/
+    # URI schemes to accept for extraction.
+    URI_SCHEMES = %w(http https).freeze
 
-# Regex to select lines starting with "HH:MM " time.
-HH_MM = /^([0-1][0-9]|[2][0-3]):[0-5][0-9] .*/
+    # Trailing chars (all punctuation except "/") to remove from URIs.
+    TRAILING = /[^\/[:^punct:]]+$/
 
-# Regex to select IRC <nick>.
-IRC_NICK = /<.+?>/
+    # Regex to select lines starting with "HH:MM " time.
+    HH_MM = /^([0-1][0-9]|[2][0-3]):[0-5][0-9] .*/
 
-# Regex to select "<" and ">" chars.
-LT_GT = /[<>]/
+    # Regex to select IRC <nick>.
+    IRC_NICK = /<.+?>/
 
-# Maximum digits to display for log lines.
-LINE_DIGITS = 3
+    # Regex to select "<" and ">" chars.
+    LT_GT = /[<>]/
 
-# Length of timestamp string being used.
-TIME_SIZE = 'HH:MM'.size
-TIME_SIZE_PLUS_1 = TIME_SIZE + 1 # Micro-perf optimization
+    # Maximum digits to display for log lines.
+    LINE_DIGITS = 3
 
-NON_BREAKING_SPACE = '&nbsp;'
+    # Length of timestamp string being used.
+    TIME_SIZE = 'HH:MM'.size
+    TIME_SIZE_PLUS_1 = TIME_SIZE + 1 # Micro-perf optimization
 
-# Convert logs from plain text to HTML with line number links.
-#
-Jekyll::Hooks.register :documents, :pre_render do |post|
+    NON_BREAKING_SPACE = '&nbsp;'
 
-  # Loop through each line of the meeting logs.
-  post.content.gsub!(HH_MM).with_index(1) do |line, index|
-
-    # Separate the log line into useful parts.
-    lineno  = "#{NON_BREAKING_SPACE * (LINE_DIGITS - index.to_s.size)}#{index}"
-    time    = line[0..TIME_SIZE]
-    name    = IRC_NICK.match(line).to_s
-    nick    = name.gsub(LT_GT, '').strip
-    message = CGI.escapeHTML(line[TIME_SIZE_PLUS_1 + name.size..-1])
-
-    # Extract URIs from the message and convert them to HTML links.
-    URI.extract(message, schemes = URI_SCHEMES).each do |uri|
-      link = uri.sub(TRAILING, '') # Strip unwanted trailing punctuation
-      message.sub!(link, "<a href='#{link}' target='blank'>#{link}</a>")
+    def initialize(tag_name, text, tokens)
+      super
     end
 
-    # Return the log line as HTML markup.
-    "<table class='log-line' id='l-#{index}'>" \
-      "<tr class='log-row'>" \
-        "<td class='log-lineno'><a href='#l-#{index}'>#{lineno}</a></td>" \
-        "<td class='log-time'>#{time}</td>" \
-        "<td>" \
-          "<span class='log-nick'>&lt;#{nick}&gt;</span>" \
-          "<span class='log-msg'>#{message}</span>" \
-        "</td>" \
-      "</tr>" \
-    "</table>"
+    def render(context)
+      output = super
+
+      # Loop through each line of the meeting logs.
+      output.gsub!(HH_MM).with_index(1) do |line, index|
+
+        # Separate the log line into useful parts.
+        lineno  = "#{NON_BREAKING_SPACE * (LINE_DIGITS - index.to_s.size)}#{index}"
+        time    = line[0..TIME_SIZE]
+        name    = IRC_NICK.match(line).to_s
+        nick    = name.gsub(LT_GT, '').strip
+        message = CGI.escapeHTML(line[TIME_SIZE_PLUS_1 + name.size..-1])
+
+        # Extract URIs from the message and convert them to HTML links.
+        URI.extract(message, schemes = URI_SCHEMES).each do |uri|
+          link = uri.sub(TRAILING, '') # Strip unwanted trailing punctuation
+          message.sub!(link, "<a href='#{link}' target='blank'>#{link}</a>")
+        end
+
+        # Return the log line as HTML markup.
+        "<table class='log-line' id='l-#{index}'>" \
+          "<tr class='log-row'>" \
+            "<td class='log-lineno'><a href='#l-#{index}'>#{lineno}</a></td>" \
+            "<td class='log-time'>#{time}</td>" \
+            "<td>" \
+              "<span class='log-nick'>&lt;#{nick}&gt;</span>" \
+              "<span class='log-msg'>#{message}</span>" \
+            "</td>" \
+          "</tr>" \
+        "</table>"
+      end
+
+      output
+    end
   end
 end
+
+Liquid::Template.register_tag('irc', Jekyll::IRCBlock)
